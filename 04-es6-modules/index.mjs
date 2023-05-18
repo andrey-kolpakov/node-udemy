029a064877003703eb40963f0d5837a3a83285de
Andrey Kolpakov <gfstels@yandex.ru>
2023-05-18 09:23:12 +0600

    new

--- 04-es6-modules/index.mjs
+++ 04-es6-modules/index.mjs
@@ -1,9 +1,13 @@
 import { season, temperature } from './named-exports.mjs'
 import { isRaining, humidity } from './inline-exports.mjs'
 
+import USERNAME, { PASSWORD } from './mixed-exports.mjs'
+
 //Можно назвать как угодно
 import newValueOtherName from './export-default.mjs'
 
 console.log(season, temperature)
 console.log(isRaining, humidity)
 console.log(newValueOtherName)
+
+console.log(USERNAME, PASSWORD)
---
+++ 04-es6-modules/mixed-exports.mjs
@@ -0,0 +1,6 @@
+const USERNAME = 'admin'
+const PASSWORD = 'strong-password'
+
+export default USERNAME
+
+export {PASSWORD}
